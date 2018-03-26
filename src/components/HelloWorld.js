b3bc425f44446c684cf6b2648abc97535c17dd64
Cesar Salazar <cesarsalazar09@gmail.com>
2018-03-25 23:46:15 -0500

    added HelloWorld Component

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+
+class HelloWorld extends Component {
+
+    constructor(){
+        super();
+    }
+
+
+    render(){
+        return (
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        )
+    }
+
+
+}
+
+
+export default HelloWorld;
