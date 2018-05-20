5175be91d7467bc3ba490730291097cd33bfc3da
Cesar Salazar <cesarsalazar09@gmail.com>
2018-05-19 22:28:06 -0500

    top & bottom captions display on meme creation

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import {Text, TouchableOpacity} from 'react-native';
-import {TextInput, Image, Tile, Screen, Button, Icon} from '@shoutem/ui';
+import {TextInput, ImageBackground, Tile, Screen, Button, Icon, Overlay, Subtitle, Heading, Title, View} from '@shoutem/ui';
+import {captureRef} from 'react-native-view-shot';
 
 
 class MemeGenerator extends Component {
     constructor(props){
         super(props);
+
+        this.state= {
+            top_text: "",
+            bottom_text: ""
+        };
     }
 
 
@@ -16,15 +22,26 @@ class MemeGenerator extends Component {
             <Screen>
                 <TextInput 
                     placeholder={"top caption text"}
+                    onChangeText={(text) => this.setState({top_text: text})}
+                    value={this.state.top_text}
                 />
-                <Tile>
-                    <Image
-                        styleName='large-wide'
-                        source={{ uri: navigation.getParam('image_source')}}
-                    />
-                </Tile>
+
+                <ImageBackground
+                    
+                    styleName='large-wide'
+                    source={{ uri: navigation.getParam('image_source')}}
+                > 
+                    <Tile styleName="text-centric">
+                        <View styleName="vertical fill-parent space-between">
+                            <Title styleName="v-start" style={{color: "white", fontSize: 20, fontFamily: 'Roboto', textAlign: "center"}}>{this.state.top_text}</Title>
+                            <Title styleName="v-end" style={{color: "white", fontSize: 20, fontFamily: 'Roboto', textAlign: "center"}}>{this.state.bottom_text}</Title>
+                        </View>
+                    </Tile>
+                </ImageBackground>
                 <TextInput 
                     placeholder={"bottom caption text"}
+                    onChangeText={(text) => this.setState({bottom_text: text})}
+                    value={this.state.bottom_text}
                 />
                 <Button styleName="full-width">
                     <Icon name="plus-button" />
