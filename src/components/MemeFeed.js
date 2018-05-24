05861500a4cbf240fb5395ad5f22f89ad12c506a
Cesar Salazar <cesarsalazar09@gmail.com>
2018-05-23 21:35:32 -0500

    got MemeFeed to pull data and display memes from firebase backend

--- src/components/MemeFeed.js
+++ src/components/MemeFeed.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Text, FlatList, Dimensions} from 'react-native';
 import { Icon, Card} from 'react-native-elements';
 import { Image, Tile, View, Button, NavigationBar, Title} from '@shoutem/ui';
-
+import firebase from 'react-native-firebase';
 
 
 
@@ -11,26 +11,70 @@ class MemeFeed extends Component {
 
     constructor(props){
         super(props);
+
+
+        this.state = {
+            authenticated: false,
+            data: [{key: "1", title: 'still waiting', image: ""}]
+        }
+
+    
+    }
+
+    componentDidMount(){
+
+        this.login();
+        this.fetchFireBaseData();
+
+    }
+
+
+
+    login(){
+        if(this.state.authenticated !== true){
+            firebase.auth().signInAndRetrieveDataWithEmailAndPassword('cesarsalazar09@gmail.com', 'test123')
+            .then((value) => this.setState({authenticated: true}),
+            (error) => console.log(error));
+        }
+    }
+
+    fetchFireBaseData(){
+
+        firebase.firestore().collection('memes')
+            .get()
+            .then((querysnapshot) => this.populateList(querysnapshot));
+    }
+
+    populateList(querysnapshot){
+        dataReceived = [];
+        querysnapshot.forEach(element => {
+            dataReceived.push(element.data());
+        });
+
+        console.log(dataReceived);
+
+        this.setState({data: dataReceived});
+        
     }
 
 
     render(){
 
-        const fake_data = [
-            {key: "1", title: 'meme1', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "2", title: 'meme2', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "3", title: 'meme3', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "4", title: 'meme4', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "5", title: 'meme5', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "6", title: 'meme6', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "7", title: 'meme7', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "8", title: 'meme8', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "9", title: 'meme9', source: require('../assets/images/jesus-meme.jpg')},
-            {key: "10", title: 'meme10', source: require('../assets/images/jesus-meme.jpg')},
-        ];
+        // const fake_data = [
+        //     {key: "1", title: 'meme1', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "2", title: 'meme2', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "3", title: 'meme3', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "4", title: 'meme4', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "5", title: 'meme5', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "6", title: 'meme6', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "7", title: 'meme7', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "8", title: 'meme8', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "9", title: 'meme9', source: require('../assets/images/jesus-meme.jpg')},
+        //     {key: "10", title: 'meme10', source: require('../assets/images/jesus-meme.jpg')},
+        // ];
 
-        let screen_width = Dimensions.get('window').width;
-        let screen_height = Dimensions.get('window').height;
+        // let screen_width = Dimensions.get('window').width;
+        // let screen_height = Dimensions.get('window').height;
             
         return (
             // {console.log(this.props.navigation)}
@@ -41,7 +85,7 @@ class MemeFeed extends Component {
                 /> */}
                 <FlatList 
                     ListHeaderComponent={
-                        <View style={{flexDirection: 'row', backgroundColor: 'white'}}>
+                        <View style={{flexDirection: 'row', backgroundColor: 'white', justifyContent: 'flex-start'}}>
                             <Button><Icon name="menu"/></Button>
                             <Title style={{textAlign: 'center'}}>MemeU</Title>
                         </View>
@@ -51,7 +95,8 @@ class MemeFeed extends Component {
                         // />
                     }
                     stickyHeaderIndices={[0]}
-                    data={fake_data}
+                    data={this.state.data}
+                    keyExtractor={item => item.title}
                     renderItem={({item}) => 
                         <Card
                         //    style= {{width: screen_width}}
@@ -59,7 +104,7 @@ class MemeFeed extends Component {
                             >
                             <Image 
                                 styleName='large-square'
-                                source={item.source}
+                                source={item.image}
                             />
                             <View styleName="horizontal flexible">
                                 <Button styleName="border full-width">
@@ -74,7 +119,7 @@ class MemeFeed extends Component {
 
                     refreshing={false}
 
-                    onRefresh={() => console.log('this is refreshing')}
+                    onRefresh={() => this.fetchFireBaseData()}
                 />
             </View>
         )
